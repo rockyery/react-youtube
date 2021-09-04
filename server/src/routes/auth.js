574392626950949536f6db3c594bc4fed9e51680
Sebastian Briones <rockyery@gmail.com>
2021-09-04 10:49:00 +0800

    Initial commit
    Initial commit and getRecommendedVideos route

---
+++ server/src/routes/auth.js
@@ -0,0 +1,60 @@
+import express from "express";
+import {PrismaClient} from "@prisma/client"
+import jwt from "jsonwebtoken"
+import { protect } from '../middleware/authorization'
+
+const prisma = new PrismaClient()
+// A function to get the routes.
+// All route definitions are in one place and we only need to export one thing
+function getAuthRoutes() {
+  const router = express.Router();
+
+  // http://localhost:3001/api/v1/auth/current-user
+  router.post('/google-login', googleLogin)
+
+  router.get('/me', protect, me)
+
+  router.get('/signout', signout)
+
+  return router;
+}
+
+// All controllers/utility functions here
+async function googleLogin(req, res) {
+  const { username, email } = req.body
+
+  let user = await prisma.user.findUnique({
+    where: {
+      email:email
+    }
+  })
+
+  if(!user) {
+    user = await prisma.user.create({
+      data: {
+        username,
+        email
+      }
+    })
+  }
+
+  const tokenPayload = { id: user.id }
+  const token = jwt.sign(tokenPayload, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE
+  })
+
+  res.cookie('token', token, { httpOnly: true})
+  res.status(200).send(token)
+}
+
+async function me(req, res) {
+  console.log(req.user)
+  res.status(200).json( {user: req.user} )
+}
+
+function signout(req, res) {
+  res.clearCookie('token')
+  res.status(200).json({})
+}
+
+export { getAuthRoutes };
---
+++ server/src/routes/video.js
@@ -0,0 +1,67 @@
+import express from "express";
+import {PrismaClient} from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+function getVideoRoutes() {
+  const router = express.Router();
+
+  router.get('/', getRecommendedVideos)
+
+  return router;
+}
+
+async function getVideoViews(videos) {
+  for(const video of videos) {
+    const views = await prisma.view.count({
+      where: {
+        videoId: {
+          equals: video.id
+        }
+      }
+    })
+    video.views = views
+  }
+  return videos
+}
+
+async function getRecommendedVideos(req, res) {
+  let videos = await prisma.video.findMany({
+    include: {
+      user: true
+    },
+    orderBy: {
+      createdAt: "desc"
+    }
+  })
+
+  if(!videos.length) {
+    return res.status(200).json({ videos })
+  }
+
+  videos = await getVideoViews(videos)
+
+  res.status(200).json({ videos })
+}
+
+async function getTrendingVideos(req, res) {}
+
+async function searchVideos(req, res, next) {}
+
+async function addVideo(req, res) {}
+
+async function addComment(req, res, next) {}
+
+async function deleteComment(req, res) {}
+
+async function addVideoView(req, res, next) {}
+
+async function likeVideo(req, res, next) {}
+
+async function dislikeVideo(req, res, next) {}
+
+async function getVideo(req, res, next) {}
+
+async function deleteVideo(req, res) {}
+
+export { getVideoRoutes };
