1f32019683d7f4195fc2428c8a136deaae4aaf61
Sebastian Briones <rockyery@gmail.com>
2021-09-11 19:23:42 +0800

    Delete Video by Id
    Delete Video by Id

--- server/src/routes/video.js
+++ server/src/routes/video.js
@@ -11,7 +11,7 @@ function getVideoRoutes() {
   router.get('/trending', getTrendingVideos)
   router.get('/search', searchVideos)
   router.get('/:videoId', getAuthUser, getVideo)
-
+  router.delete('/:videoId', protect, deleteVideo)
 
   router.post('/', protect, addVideo)
   router.get('/:videoId/view', getAuthUser, addVideoView)
@@ -453,8 +453,6 @@ async function getVideo(req, res, next) {
       }
     })
 
-    console.log('isDisliked ' + isDisliked);
-
     isViewed = await prisma.view.findFirst({
       where : {
         userId: {
@@ -532,6 +530,51 @@ async function getVideo(req, res, next) {
   res.status(200).json({ video })
 }
 
-async function deleteVideo(req, res) { }
+async function deleteVideo(req, res) { 
+  const video = await prisma.video.findUnique({
+    where: {
+      id: req.params.videoId
+    },
+    select: {
+      userId: true
+    }
+  })
+
+  if(req.user.id !== video.userId) {
+    return res.status(401).send('You are not authorized to delete this video')
+  }
+
+  await prisma.view.deleteMany({
+    where: {
+      videoId: {
+        equals: req.params.videoId
+      }
+    }
+  })
+
+  await prisma.videoLike.deleteMany({
+    where: {
+      videoId: {
+        equals: req.params.videoId
+      }
+    }
+  })
+
+  await prisma.comment.deleteMany({
+    where: {
+      videoId: {
+        equals: req.params.videoId
+      }
+    }
+  })
+
+  await prisma.video.delete({
+    where: {
+      id: req.params.videoId
+    }
+  })
+
+  res.status(200).json({})
+}
 
 export { getVideoRoutes }
