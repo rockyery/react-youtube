a6931278b2a158a26664f92350e5ccb010035ec7
Sebastian Briones <rockyery@gmail.com>
2021-10-11 20:53:36 +0800

    Update user endpoint
    Update user endpoint

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -8,6 +8,8 @@ function getUserRoutes() {
   const router = express.Router();
 
   router.get('/', protect, getRecommendedChannels)
+  router.put('/', protect, editUser)
+
   router.get('/liked-videos', protect, getLikedVideos)
   router.get('/history', protect, getHistory)
   router.get('/:userId', getAuthUser, getProfile)
@@ -362,7 +364,17 @@ async function getProfile(req, res, next) {
 }
 
 async function editUser(req, res) {
+  const { username, cover, avatar, about} = req.body
+  const user = await prisma.user.update({
+    where: {
+      id: req.user.id
+    },
+    data: {
+      username, cover, avatar, about
+    }
+  })
 
+  res.status(200).json({user})
 }
 
 export { getUserRoutes };
